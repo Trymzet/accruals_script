b4caab9e722b6b9e08f5fa8b86b0a67e9a5532df
Michał Zawadzki <michalmzawadzki@gmail.com>
2017-09-09 21:54:16 +0200

    Beginning to fill out the form using the grouped data.

--- pivot.py
+++ pivot.py
@@ -2,20 +2,37 @@ import pandas as pd
 import openpyxl as oxl
 
 
-def generate_csv(cc): # cc = Company Code
+def generate_csv(cc):  # cc = Company Code
 
     CSV_file_name = "../Script/Result/to_upload/{}_Accrual_WD_{}.csv".format(cc, date_cut)
     # generate the form
     JE_csv = pd.DataFrame(columns=JE_csv_columns)
-
     cur_cc_data = grouped_by_cc.get_group(cc)
-    print("AAAAAAAAAAAA")
-    #print(cur_cc_data)
-    grouped_by_checksum = cur_cc_data.groupby(["Checksum", "Expense Report Number"])
-    for k, g in grouped_by_checksum:
-        print(g)
+    grouped_by_checksum = cur_cc_data.groupby(["Checksum"])
+    posting_month = last_day_of_previous_month.strftime("%b")
+    posting_year = last_day_of_previous_month.strftime("%Y")
 
-    # here put the code for filling out the template
+    start_row = 0
+    for k, g in grouped_by_checksum:
+        #print(k)
+        #print(g)
+        for i in range(start_row, start_row + len(g)):
+            # for each line for a given checksum (BA and PC combination), retrieve its Acc# culumn value and input it
+            # into the next free cell in the "ACCOUNT" column in the JE csv form
+            JE_csv.loc[i, "ACCOUNT"] = g.iloc[i - start_row]["Acc#"]
+            JE_csv.loc[i, "DEBIT"] = g.iloc[i - start_row]["Net Amount LC"]
+            JE_csv.loc[i, "LINE MEMO"] = g.iloc[i - start_row]["Expense Report Number"] + " Accrual"
+            JE_csv.loc[i, "BUSINESS AREA"] = g.iloc[i - start_row]["Checksum"][-4:]  # BA is the last 4 chars of checksum
+            JE_csv.loc[i, "PROFIT CENTER"] = g.iloc[i - start_row]["Checksum"][:5]  # PC is the first 5 chars of checksum
+
+            # make sure this is correctly read by Excel
+            JE_csv.loc[i, "DATE"] = last_day_of_previous_month
+
+            JE_csv.loc[i, "SUBSIDIARY"] = cc
+            JE_csv.loc[i, "MEMO"] = "WD {} ACCRUALS {} FY{}".format(cc, posting_month, posting_year)
+        start_row += len(g)
+    if cc == "BESA":
+        print(JE_csv)
 
     #JE_csv.to_csv(CSV_file_name, index=False)
 
