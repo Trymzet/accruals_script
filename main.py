9b34b62ecf1e51fb1669340e2820e6c91b7bd5d4
Michał Zawadzki <michalmzawadzki@gmail.com>
2017-09-12 22:55:40 +0200

    Major performance improvements, minor "stability issues" :)
    - added the generate_vbs_script() function from StackOverflow ("Faster way to read Excel files to pandas dataframe")
    - added load_csv() function using pd.read_csv() instead of pd.read_excel()
    - rewrote the load_all() function to utilize the new loading function, as it is 4-5x faster than using xlsx (3 minutes to 40 seconds).
    - will have to specify some dtypes as pd.read_csv() infers them differently than pd.read_excel()...

--- main.py
+++ main.py
@@ -4,13 +4,72 @@
 import pandas as pd
 import openpyxl
 from subprocess import call
+from os import remove
 
 WD_report_name = "EXP031-RPT-Process-Accruals_with_Expense_Report.xlsx"
 WD2_report_name = "EXP032-RPT-Process-Accruals-_No_Expense.xlsx"
 AP_account = 25702400  # the account from which the money will flow
 generic_GL_account = 46540000
 
-# try with read_csv in the future, as it's faster
+
+def generate_vbs_script():
+    vbscript = """if WScript.Arguments.Count < 3 Then
+        WScript.Echo "Please specify the source and the destination files. Usage: ExcelToCsv <xls/xlsx source file> <csv destination file> <worksheet number (starts at 1)>"
+        Wscript.Quit
+    End If
+
+    csv_format = 6
+
+    Set objFSO = CreateObject("Scripting.FileSystemObject")
+
+    src_file = objFSO.GetAbsolutePathName(Wscript.Arguments.Item(0))
+    dest_file = objFSO.GetAbsolutePathName(WScript.Arguments.Item(1))
+    worksheet_number = CInt(WScript.Arguments.Item(2))
+
+    Dim oExcel
+    Set oExcel = CreateObject("Excel.Application")
+
+    Dim oBook   
+    Set oBook = oExcel.Workbooks.Open(src_file)
+    oBook.Worksheets(worksheet_number).Activate
+
+    oBook.SaveAs dest_file, csv_format
+
+    oBook.Close False
+    oExcel.Quit
+    """
+
+    with open("ExcelToCsv.vbs", "wb") as f:
+        f.write(vbscript.encode("utf-8"))
+
+
+def load_csv(excel_file_name, has_sheets=False, skiprows=None):
+    if has_sheets:
+        # sheet numbers to use; using the first three, hence the fixed numbers; fifth sheet in Master file is the template,
+        # which has to be parsed as xlsx
+        sheets = map(str, range(1, 4))
+        sheet_dataframes = []
+        for sheet in sheets:
+            csv_file_name = "../Script/{}{}".format(sheet, ".csv")
+            call(["cscript.exe", "../Script/ExcelToCsv.vbs", excel_file_name, csv_file_name, sheet, r"//B"])
+            sheet_dataframe = pd.read_csv(csv_file_name, encoding="latin-1")
+            sheet_dataframes.append(sheet_dataframe)
+        return tuple(sheet_dataframes)
+    else:
+        csv_file_name = excel_file_name[:-4] + "csv"
+        # //B is for batch mode; this is to avoid spam on the console
+        call(["cscript.exe", "../Script/ExcelToCsv.vbs", excel_file_name, csv_file_name, str(1), r"//B"])
+        if skiprows:
+            try:
+                data = pd.read_csv(csv_file_name, skiprows=skiprows, encoding="latin-1")
+            except:
+                print("Something went wrong... make sure report names weren't changed or debug the load_csv function")
+        else:
+            try:
+                data = pd.read_csv(csv_file_name, encoding="latin-1")
+            except:
+                print("Something went wrong... make sure report names weren't changed or debug the load_csv function")
+        return data
 
 
 def load_workbook(file_name, sheetname=None, skiprows=None):
@@ -32,34 +91,31 @@ def load_workbook(file_name, sheetname=None, skiprows=None):
     return wb
 
 
-
-
-
-
 def load_all():
     master_file_name = "WD_Accruals_Master.xlsm"
-    accounts_sheet_name = "GL_accounts_by_category"
-    ba_pc_sheet_name = "CC_to_BA_PC"
-    JE_template_sheet_name = "FAST JE Template"
-
-    file_names = [WD_report_name, WD2_report_name, accounts_sheet_name, ba_pc_sheet_name, JE_template_sheet_name]
+    file_names = [WD_report_name, WD2_report_name, master_file_name]
     dataframes = []
+
+    # this will be used by each load_csv() call to convert every .xlsx to .csv for faster loading
+    generate_vbs_script()
 
     for file_name in file_names:
         if file_name == WD_report_name:
-            df = load_workbook(file_name, skiprows=[0])
-            dataframes.append(df)
+            df = load_csv(file_name, skiprows=[0])
         elif file_name == WD2_report_name:
-            df = load_workbook(file_name)
-            dataframes.append(df)
+            df = load_csv(file_name)
         else:
-            df = load_workbook(master_file_name, sheetname=file_name)
-            dataframes.append(df)
+            sheet1, sheet2, sheet3 = load_csv(file_name, has_sheets=True)
+            dataframes.extend([sheet1, sheet2, sheet3])
+            return dataframes
+        dataframes.append(df)
     return dataframes
 
 
 def initial_cleanup():
     global WD_report, WD2_report
+    # no longer needed
+    remove("ExcelToCsv.vbs")
     # remove rows with total amount 0 or less
     WD_report = WD_report[WD_report["Net Amount LC"] > 0]
     WD2_report = WD2_report[WD2_report["Billing Amount"] > 0]
@@ -123,7 +179,7 @@ def final_cleanup():
     duplicates.to_csv("duplicates.xlsx")
 
 
-WD_report, WD2_report, accounts_file, ba_pc_file, JE_template = load_all()
+WD_report, WD2_report, ba_pc_file, accounts_file, JE_template_file = load_all()
 print("All files loaded :)")
 initial_cleanup()
 print("Data cleaned :)")
